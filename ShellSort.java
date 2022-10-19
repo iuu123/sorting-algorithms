1bc5a287b2ff4f8113df4b91eeb64b22511a3c6a
duheyu <leiyue159@gmail.com>
2022-10-19 23:37:09 +0800

    jfa

---
+++ ShellSort.java
@@ -0,0 +1,28 @@
+package sort;
+
+import java.util.Arrays;
+
+public class ShellSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {52, 896, 546, 23, 46, 853};
+		shell(arr);
+		System.out.println(Arrays.toString(arr));
+	}
+	
+	public static void shell(int[] arr) {
+		int val = 0;
+		int j = 0;
+		for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+			for (int i = gap; i < arr.length; i++) {
+				val = arr[i];
+				for (j = i; j >= gap && val < arr[j - gap]; j -= gap) {
+					arr[j] = arr[j - gap];
+				}
+				arr[j] = val;
+			}
+		}
+	}
+
+}
